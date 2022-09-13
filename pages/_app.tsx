ab2204c694f0d3b514dbbd9405edee87ffe99a06
박민한 <alsgks1218@naver.com>
2022-09-13 14:50:14 +0900

    dev 브랜치 작업

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/router";
 import NavBar from "../components/NavBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  console.log("dev 브랜치");
   Axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL + "/api";
   Axios.defaults.withCredentials = true;
 
