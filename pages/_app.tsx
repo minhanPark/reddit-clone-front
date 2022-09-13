3a801f45c443041492eaeac0e5197d8216b91b26
박민한 <alsgks1218@naver.com>
2022-09-13 15:23:53 +0900

    메인 브랜치 테스트

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,8 @@ import { useRouter } from "next/router";
 import NavBar from "../components/NavBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  console.log("dev 브랜치");
-  console.log("dev dev");
+  console.log("main 브랜치");
+  console.log("main main");
   Axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL + "/api";
   Axios.defaults.withCredentials = true;
 
